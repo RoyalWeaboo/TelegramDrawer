f0730e12e955996f17af2005db99dd06d168b085
RoyalWeaboo <malikazizali86@gmail.com>
2022-09-05 14:13:12 +0700

    Initial commit

---
+++ app/src/main/java/com/malikazizali/telegramdrawerapp/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.malikazizali.telegramdrawerapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.malikazizali.telegramdrawerapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    lateinit var drawerLayout: DrawerLayout
+    private lateinit var mToggle : ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        binding.appBarMain.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+
+        drawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_group,
+                R.id.nav_contacts,
+                R.id.nav_calls,
+                R.id.nav_nearby,
+                R.id.nav_saved_messages,
+                R.id.nav_settings,
+                R.id.nav_invite,
+                R.id.nav_features
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        mToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(mToggle)
+        mToggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        return mToggle.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_group -> {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.nav_host_fragment_content_main, NewGroupFragment())
+                    commit()
+                }
+                true
+            }
+            R.id.nav_contacts -> {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.nav_host_fragment_content_main, ContactsFragment())
+                    commit()
+                }
+                true
+            }
+            R.id.nav_calls -> {
+                supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.nav_host_fragment_content_main, CallsFragment())
+                    commit()
+                }
+                true
+            }
+            R.id.nav_nearby -> {
+
+            }
+            R.id.nav_saved_messages -> {
+
+            }
+            R.id.nav_settings -> {
+
+            }
+            R.id.nav_invite -> {
+
+            }
+            R.id.nav_features -> {
+
+            }
+            else -> true
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+}
